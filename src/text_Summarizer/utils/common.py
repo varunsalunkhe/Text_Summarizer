62d0ca61232b6ba15cdd6f04e2d24703a4d5ee96
Varun Salunkhe <varunsalunkhe07@gmail.com>
2025-01-18 11:04:22 +0530

    updated files added.

--- src/text_Summarizer/utils/common.py
+++ src/text_Summarizer/utils/common.py
@@ -1,13 +1,13 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from text_Summarizer.logging import logger 
-from ensure import ensure_annotations
+from text_Summarizer.logging import logger
+# from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
-@ensure_annotations
+# @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     """reads yaml file and returns
 
@@ -33,7 +33,7 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     
 
 
-@ensure_annotations
+# @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
 
@@ -48,7 +48,7 @@ def create_directories(path_to_directories: list, verbose=True):
 
 
 
-@ensure_annotations
+# @ensure_annotations
 def get_size(path: Path) -> str:
     """get size in KB
 
