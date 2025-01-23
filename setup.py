116dc64266eba32068588f9d170c35a2f6807018
Varun Salunkhe <varunsalunkhe07@gmail.com>
2025-01-23 18:12:40 +0530

    updated files added.

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
 __version__ = "0.0.0"
 
 REPO_NAME = "Text_Summarizer"
 AUTHOR_USER_NAME = "varunsalunkhe"
 SRC_REPO = "text_Summarizer"
 AUTHOR_EMAIL = "varunsalunkhe07@gmail.com"
-
 
 
 setuptools.setup(
